50a084c70f9854ba6040f008526104f0326a0aa2
Archer <tanrui106@163.com>
2018-11-01 23:39:59 +0800

    更新Sample2

--- Code/src/CookerFactory.java
+++ Code/src/CookerFactory.java
@@ -1,12 +1,12 @@
 
 public class CookerFactory{
 
-//    public AbstractCooker createCooker(Sample.CookerType type){
-//        switch(type){
-//
-//
-//        }
-//    }
-//
+   public AbstractCooker createCooker(Sample.CookerType type){
+       switch(type){
+            case Sample.CookerType
+
+       }
+   }
+
 
 }
